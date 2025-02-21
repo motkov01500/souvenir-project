738a0030a9c5b17907f6d05ea9be43c52678d8a0
motkov01500 <atanas.peshev@partner.bmw.de>
2025-02-21 20:21:26 +0200

    add BE initial

---
+++ souvenir-be/src/main/java/com/mondeshko/souvenir/SouvenirApplication.java
@@ -0,0 +1,13 @@
+package com.mondeshko.souvenir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouvenirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouvenirApplication.class, args);
+	}
+
+}
---
+++ souvenir-be/src/test/java/com/mondeshko/souvenir/SouvenirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mondeshko.souvenir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouvenirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
